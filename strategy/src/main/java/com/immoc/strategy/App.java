c7beb1ef86f1f47c9d02ad6893048a446721d8c2
xu <804288658@qq.com>
2017-09-09 12:57:08 +0800

    strategy

---
+++ strategy/src/main/java/com/immoc/strategy/App.java
@@ -0,0 +1,14 @@
+package com.immoc.strategy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!2" );
+    }
+}
